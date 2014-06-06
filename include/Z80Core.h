e7ea616f82011b8b649e276fb667f1533d735ad2
nnarain <nnarain101@gmail.com>
2014-06-06 09:32:47 -0400

    Add definitions for memory map regions

--- include/Z80Core.h
+++ include/Z80Core.h
@@ -13,10 +13,37 @@
 
 #include <cstdint>
 
-#define SIZE_MEMORY_MAP  0xFFFF + 1
+#define SIZE_MEMORY_MAP              0xFFFF + 1
 
-#define INTERRUPT_ENABLE 0xFFFF
-#define INTERRUPT_FLAG   0xFF0F
+#define PERMANENT_ROM_BANK_START     0x0000
+#define PERMANENT_ROM_BANK_END       0x3FFF
+
+#define SWITCHABLE_ROM_BANK_START    0x4000
+#define SWITCHABLE_ROM_BANK_END      0x7FFF
+
+#define VRAM_START                   0x8000
+#define VRAM_END                     0x9FFF
+
+#define EXTERNAL_RAM_BANK_START      0xA000
+#define EXTERNAL_RAM_BANK_END        0xBFFF
+
+#define WORK_RAM_BANK_0_START        0xC000
+#define WORK_RAM_BANK_0_END          0xCFFF
+
+#define WORK_RAM_BANK_1_START        0xD000
+#define WORK_RAM_BANK_1_END          0xDFFF
+
+#define SPRITE_ATTRIBUTE_TABLE_START 0xFE00
+#define SPRITE_ATTRIBUTE_TABLE_END   0xFEFF
+
+#define DEVICE_MAP_START             0xFF00
+#define DEVICE_MAP_END               0xFF7F
+
+#define HIGH_RAM_START               0xFF80
+#define HIGH_RAM_END                 0xFFFE
+
+#define INTERRUPT_ENABLE             0xFFFF
+#define INTERRUPT_FLAG               0xFF0F
 
 #ifdef __cplusplus
 namespace Z80{
