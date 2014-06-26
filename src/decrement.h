a35ef1e3fd1cd7c96bfd739853c955d1e4479017
Natesh Narain <nnarain@blackberry.com>
2014-06-26 10:59:12 -0400

    moved decrement instructions

---
+++ src/decrement.h
@@ -0,0 +1,14 @@
+
+#ifndef DECREMENT
+#define DECREMENT
+
+int decA(struct Core*);
+int decB(struct Core*);
+int decC(struct Core*);
+int decD(struct Core*);
+int decE(struct Core*);
+int decH(struct Core*);
+int decL(struct Core*);
+int decHL(struct Core*);
+
+#endif
