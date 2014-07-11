fec37887aed1b0fdb6761bc1fae9d1c0811e1276
Natesh Narain <nnarain@blackberry.com>
2014-07-11 12:57:56 -0400

    implemented subtract with borrow

--- src/subtract.h
+++ src/subtract.h
@@ -14,5 +14,15 @@ int subAL(struct Core*);
 int subAHL(struct Core*);
 int subAByte(struct Core*);
 
+int subcAA(struct Core*);
+int subcAB(struct Core*);
+int subcAC(struct Core*);
+int subcAD(struct Core*);
+int subcAE(struct Core*);
+int subcAH(struct Core*);
+int subcAL(struct Core*);
+int subcA_HL_(struct Core*);
+int subcAByte(struct Core*);
+
 
 #endif
