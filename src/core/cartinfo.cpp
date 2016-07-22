b5670862297215ec162d6446773490d6ba2a1ad2
Natesh Narain <nnaraindev@gmail.com>
2016-07-22 19:14:08 -0400

    print type, ram size and rom size

--- src/core/cartinfo.cpp
+++ src/core/cartinfo.cpp
@@ -13,14 +13,13 @@ namespace gb
     {
         CartInfo info;
         info.type = image[memorymap::CART_TYPE];
-        info.ram_size = image[memorymap::CART_ROM_SIZE];
+        info.rom_size = image[memorymap::CART_ROM_SIZE];
         info.ram_size = image[memorymap::CART_RAM_SIZE];
         std::memcpy(
             info.game_title,
             &image[memorymap::GAME_TITLE_START],
             memorymap::GAME_TITLE_END - memorymap::GAME_TITLE_START
         );
-
 
         return info;
     }
