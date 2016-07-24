3b161bafac863cd25ccba404044299d81c9f89f0
Natesh Narain <nnaraindev@gmail.com>
2016-07-23 21:16:12 -0400

    fixed lookup table

--- src/core/mmu.cpp
+++ src/core/mmu.cpp
@@ -37,7 +37,7 @@ namespace gb
 	void MMU::loadROMBanks(uint8_t rom_size, uint8_t * rom)
 	{
 		static unsigned int rom_banks1[] = {
-			0, 4, 8, 16, 32, 64, 128, 256 
+			2, 4, 8, 16, 32, 64, 128, 256 
 		};
 		static unsigned int rom_banks2[] = {
 			72, 80, 96
@@ -51,13 +51,12 @@ namespace gb
 		{
 			// look up the total number of banks this cartridge has
 			unsigned int cartridge_rom_banks = rom_banks1[rom_size];
+			// the number of switchable ROM banks is 2 less than the total
+			// since there are always 2 available in the $0000 - $3FFF range
+			unsigned int switchable_rom_banks = cartridge_rom_banks - 2;
 
 			if (cartridge_rom_banks != 0) 
 			{
-				// the number of switchable ROM banks is 2 less than the total
-				// since there are always 2 available in the $0000 - $3FFF range
-				unsigned int switchable_rom_banks = cartridge_rom_banks - 2;
-
 				rom_banks_.resize(switchable_rom_banks);
 				copyROMToBanks(switchable_rom_banks, rom);
 			}
