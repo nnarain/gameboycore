dd6b25985191285fc89c72c17d6135a5fdc9b48c
Natesh Narain <nnaraindev@gmail.com>
2016-09-05 21:51:01 -0400

    shift instructions

--- src/core/shiftrotate.h
+++ src/core/shiftrotate.h
@@ -13,22 +13,34 @@ namespace gb
 	/**
 		Rotate bits left and set carry flags
 	*/
-	uint8_t rotateLeft(uint8_t val, uint8_t& flags);
+	uint8_t rotateLeft(uint8_t val, uint8_t n, uint8_t& flags);
 
 	/**
 		Rotate bits left through the carry flag
 	*/
-	uint8_t rotateLeftCarry(uint8_t val, uint8_t& flags);
+	uint8_t rotateLeftCarry(uint8_t val, uint8_t n, uint8_t& flags);
 
 	/**
 		Rotate bits right adn set carry flag
 	*/
-	uint8_t rotateRight(uint8_t val, uint8_t& flags);
+	uint8_t rotateRight(uint8_t val, uint8_t n, uint8_t& flags);
 
 	/**
 		Rotate bits right through carry flag
 	*/
-	uint8_t rotateRightCarry(uint8_t val, uint8_t& flags);
+	uint8_t rotateRightCarry(uint8_t val, uint8_t n, uint8_t& flags);
+
+	/**
+		Shift bits left
+	*/
+	uint8_t shiftLeft(uint8_t val, uint8_t n, uint8_t& flags);
+
+	/**
+		Shift bits right. Keep sign bit
+	*/
+	uint8_t shiftRightA(uint8_t val, uint8_t n, uint8_t& flags);
+
+	uint8_t shiftRightL(uint8_t val, uint8_t n, uint8_t& flags);
 }
 
 #endif
--- tests/test_shift_rotate_instructions.cpp
+++ tests/test_shift_rotate_instructions.cpp
@@ -11,7 +11,7 @@ TEST(Rotate, RotateLeft)
 	uint8_t flags = 0;
 	uint8_t a = 0x85;
 	
-	a = rotateLeft(a, flags);
+	a = rotateLeft(a, 1, flags);
 
 	EXPECT_EQ(a, 0x0B);
 	EXPECT_EQ(flags & CPU::Flags::C, CPU::Flags::C);
@@ -22,7 +22,7 @@ TEST(Rotate, RotateLeftThroughCarry)
 	uint8_t flags = CPU::Flags::C;
 	uint8_t a = 0x95;
 
-	a = rotateLeftCarry(a, flags);
+	a = rotateLeftCarry(a, 1, flags);
 
 	EXPECT_EQ(a, 0x2B);
 	EXPECT_EQ(flags & CPU::Flags::C, CPU::Flags::C);
@@ -33,7 +33,7 @@ TEST(Rotate, RotateRight)
 	uint8_t flags = 0;
 	uint8_t a = 0x3B;
 
-	a = rotateRight(a, flags);
+	a = rotateRight(a, 1, flags);
 
 	EXPECT_EQ(a, 0x9D);
 	EXPECT_EQ(flags & CPU::Flags::C, CPU::Flags::C);
@@ -44,8 +44,44 @@ TEST(Rotate, RotateRightThroughCarry)
 	uint8_t flags = 0;
 	uint8_t a = 0x81;
 
-	a = rotateRightCarry(a, flags);
+	a = rotateRightCarry(a, 1, flags);
 
 	EXPECT_EQ(a, 0x40);
+	EXPECT_EQ(flags & CPU::Flags::C, CPU::Flags::C);
+}
+
+TEST(Shift, Left)
+{
+	uint8_t flags = 0;
+	uint8_t a = 0x80;
+
+	a = shiftLeft(a, 1, flags);
+
+	EXPECT_EQ(a, 0x00);
+	EXPECT_EQ(flags & CPU::Flags::Z, CPU::Flags::Z);
+	EXPECT_EQ(flags & CPU::Flags::C, CPU::Flags::C);
+}
+
+TEST(Shift, RightA)
+{
+	uint8_t flags = 0;
+	uint8_t a = 0x8A;
+
+	a = shiftRightA(a, 1, flags);
+
+	EXPECT_EQ(a, 0xC5);
+	EXPECT_EQ(flags & CPU::Flags::Z, 0);
+	EXPECT_EQ(flags & CPU::Flags::C, 0);
+}
+
+TEST(Shift, RightL)
+{
+	uint8_t flags = 0;
+	uint8_t a = 0x01;
+
+	a = shiftRightL(a, 1, flags);
+
+	EXPECT_EQ(a, 0x0);
+	EXPECT_EQ(flags & CPU::Flags::Z, CPU::Flags::Z);
 	EXPECT_EQ(flags & CPU::Flags::C, CPU::Flags::C);
 }
