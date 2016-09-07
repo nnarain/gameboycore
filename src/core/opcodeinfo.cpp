b8fb5b97a93eda858adb4263f1fb7e7d377e1a4f
Natesh Narain <nnaraindev@gmail.com>
2016-09-06 22:47:14 -0400

    format string for disassembly

--- src/core/opcodeinfo.cpp
+++ src/core/opcodeinfo.cpp
@@ -5,71 +5,71 @@ namespace gb
 {
 	static const OpcodeInfo opcodeinfo1[] = {
 		{ 0, "nop",        }, // 0x00
-		{ 0, "ld BC",      }, // 0x01
+		{ 0, "ld BC,%04X",      }, // 0x01
 		{ 0, "ld (BC),A",  }, // 0x02
 		{ 0, "inc BC",     }, // 0x03
 		{ 0, "inc B",      }, // 0x04
 		{ 0, "dec B",      }, // 0x05
-		{ 0, "ld B",       }, // 0x06
+		{ 0, "ld B,%02X",       }, // 0x06
 		{ 0, "rlca",       }, // 0x07
-		{ 0, "ld (a16)SP", }, // 0x08
+		{ 0, "ld (%04X)SP", }, // 0x08
 		{ 0, "add HL,BC",  }, // 0x09
 		{ 0, "ld A,(BC)",  }, // 0x0A
 		{ 0, "dec BC",     }, // 0x0B
 		{ 0, "inc C",      }, // 0x0C
 		{ 0, "dec C",      }, // 0x0D
-		{ 0, "ld C",       }, // 0x0E
+		{ 0, "ld C,%02X",       }, // 0x0E
 		{ 0, "rrca",       }, // 0x0F
 
 		{ 0, "stop",       }, // 0x10
-		{ 0, "ld DE",      }, // 0x11
+		{ 0, "ld DE,%04X",      }, // 0x11
 		{ 0, "ld (DE),A",  }, // 0x12
 		{ 0, "inc DE",     }, // 0x13
 		{ 0, "inc D",      }, // 0x14
-		{ 0, "de cD",      }, // 0x15
-		{ 0, "ld D",       }, // 0x16
+		{ 0, "dec D",      }, // 0x15
+		{ 0, "ld D,%02X",       }, // 0x16
 		{ 0, "rla",        }, // 0x17
-		{ 0, "jr",         }, // 0x18
+		{ 0, "jr %02X",         }, // 0x18
 		{ 0, "add HL,DE",  }, // 0x19
 		{ 0, "ld A,(DE)",  }, // 0x1A
 		{ 0, "dec DE",     }, // 0x1B
 		{ 0, "inc E",      }, // 0x1C
 		{ 0, "dec E",      }, // 0x1D
-		{ 0, "ld E",       }, // 0x1E
+		{ 0, "ld E,%02X",       }, // 0x1E
 		{ 0, "rra",        }, // 0x1F
 
 		{ 0, "jr NZ",      }, // 0x20
-		{ 0, "ld HL",      }, // 0x21
+		{ 0, "ld HL,%04X",      }, // 0x21
 		{ 0, "ld (HL+),A", }, // 0x22
 		{ 0, "inc HL",     }, // 0x23
 		{ 0, "inc H",      }, // 0x24
 		{ 0, "dec H",      }, // 0x25
-		{ 0, "ld H",       }, // 0x26
+		{ 0, "ld H,%02X",       }, // 0x26
 		{ 0, "daa",        }, // 0x27
-		{ 0, "jr Z",       }, // 0x28
+		{ 0, "jr Z,%02X",       }, // 0x28
 		{ 0, "add HL,HL",  }, // 0x29
 		{ 0, "ld A,(HL+)", }, // 0x2A
 		{ 0, "de cHL",     }, // 0x2B
 		{ 0, "inc L",      }, // 0x2C
 		{ 0, "dec L",      }, // 0x2D
-		{ 0, "ld L",       }, // 0x2E
+		{ 0, "ld L,%02X",       }, // 0x2E
 		{ 0, "cpl",        }, // 0x2F
 
-		{ 0, "jr NC",      }, // 0x30
-		{ 0, "ld SP",      }, // 0x31
+		{ 0, "jr NC,%02X",      }, // 0x30
+		{ 0, "ld SP,%04X",      }, // 0x31
 		{ 0, "ld (HL-),A", }, // 0x32
 		{ 0, "inc SP",     }, // 0x33
 		{ 0, "in (HL)",    }, // 0x34
 		{ 0, "dec (HL)",   }, // 0x35
 		{ 0, "ld (HL)",    }, // 0x36
 		{ 0, "scf",        }, // 0x37
-		{ 0, "jrC",        }, // 0x38
+		{ 0, "jr C,%02X",        }, // 0x38
 		{ 0, "add HL,SP",  }, // 0x39
 		{ 0, "ld A,(HL-)", }, // 0x3A
 		{ 0, "dec SP",     }, // 0x3B
 		{ 0, "inc A",      }, // 0x3C
 		{ 0, "dec A",      }, // 0x3D
-		{ 0, "ld A",       }, // 0x3E
+		{ 0, "ld A,%02X",       }, // 0x3E
 		{ 0, "ccf",        }, // 0x3F
 
 		{ 0, "ld B,B",     }, // 0x40
@@ -210,70 +210,70 @@ namespace gb
 
 		{ 0, "ret NZ",     }, // 0xC0
 		{ 0, "pop BC",     }, // 0xC1
-		{ 0, "jp NZ",      }, // 0xC2
-		{ 0, "jp",         }, // 0xC3
-		{ 0, "call NZ",    }, // 0xC4
+		{ 0, "jp NZ,%04X",      }, // 0xC2
+		{ 0, "jp %04X",         }, // 0xC3
+		{ 0, "call NZ,%04X",    }, // 0xC4
 		{ 0, "push BC",    }, // 0xC5
-		{ 0, "add A",      }, // 0xC6
+		{ 0, "add A,%02X",      }, // 0xC6
 		{ 0, "rst 00",     }, // 0xC7
 		{ 0, "ret Z",      }, // 0xC8
 		{ 0, "ret",        }, // 0xC9
-		{ 0, "jp Z",       }, // 0xCA
+		{ 0, "jp Z,%04X",       }, // 0xCA
 	    {0, "prefix"       }, // 0xCB
-		{ 0, "call Z",     }, // 0xCC
-		{ 0, "call",       }, // 0xCD
-		{ 0, "adc A",      }, // 0xCE
+		{ 0, "call Z,%04X",     }, // 0xCC
+		{ 0, "call %04X",       }, // 0xCD
+		{ 0, "adc A,%02X",      }, // 0xCE
 		{ 0, "rst 08h",    }, // 0xCF
 
 		{ 0, "ret NC",     }, // 0xD0
 		{ 0, "pop DE",     }, // 0xD1
-		{ 0, "jp NC",      }, // 0xD2
+		{ 0, "jp NC,%04X",      }, // 0xD2
 		{ 0, "invalid"     }, // 0xD3
-		{ 0, "call NC",    }, // 0xD4
+		{ 0, "call NC,%04X",    }, // 0xD4
 		{ 0, "push DE",    }, // 0xD5
 		{ 0, "sub",        }, // 0xD6
-		{ 0, "rst10",      }, // 0xD7
+		{ 0, "rst 10",      }, // 0xD7
 		{ 0, "ret C",      }, // 0xD8
 		{ 0, "reti",       }, // 0xD9
-		{ 0, "jp C",       }, // 0xDA
+		{ 0, "jp C,%04X",       }, // 0xDA
 		{ 0, "invalid"     }, // 0xDB
-		{ 0, "call C",     }, // 0xDC
+		{ 0, "call C,%04X",     }, // 0xDC
 		{ 0, "invalid"     }, // 0xDD
-		{ 0, "sbc A",      }, // 0xDE
+		{ 0, "sbc A,%02X",      }, // 0xDE
 		{ 0, "rst 18",     }, // 0xDF
 
-		{ 0, "ldh (a8),A", }, // 0xE0
+		{ 0, "ldh (%02X),A", }, // 0xE0
 		{ 0, "pop HL",     }, // 0xE1
 		{ 0, "ld (C),A",   }, // 0xE2
 		{ 0, "invalid"     }, // 0xE3
 		{ 0, "invalid"     }, // 0xE4
 		{ 0, "push HL",    }, // 0xE5
-		{ 0, "and",        }, // 0xE6
+		{ 0, "and %02X",        }, // 0xE6
 		{ 0, "rst 20",     }, // 0xE7
-		{ 0, "add SP",     }, // 0xE8
+		{ 0, "add SP,%02X",     }, // 0xE8
 		{ 0, "jp (HL)",    }, // 0xE9
-		{ 0, "ld (a16),A", }, // 0xEA
+		{ 0, "ld (%04X),A", }, // 0xEA
 		{ 0, "invalid"     }, // 0xEB
 		{ 0, "invalid"     }, // 0xEC
 		{ 0, "invalid"     }, // 0xED
-		{ 0, "xor",        }, // 0xEE
+		{ 0, "xor %02X",        }, // 0xEE
 		{ 0, "rst 28",     }, // 0xEF
 
-		{ 0, "ldh A,(a8)",  }, // 0xF0
+		{ 0, "ldh A,(%02X)",  }, // 0xF0
 		{ 0, "pop AF",      }, // 0xF1
 		{ 0, "ld (A),C",    }, // 0xF2
 		{ 0, "di",          }, // 0xF3
 		{ 0, "invalid"      }, // 0xF4
 		{ 0, "push AF",     }, // 0xF5
-		{ 0, "or",          }, // 0xF6
+		{ 0, "or %02X",          }, // 0xF6
 		{ 0, "rst 30",      }, // 0xF7
 		{ 0, "ld HL,SP+r8", }, // 0xF8
 		{ 0, "ld SP,HL",    }, // 0xF9
-		{ 0, "ld A,(a16)",  }, // 0xFA
+		{ 0, "ld A,(%04X)",  }, // 0xFA
 		{ 0, "ei",          }, // 0xFB
 		{ 0, "invalid"      }, // 0xFC
 		{ 0, "invalid"      }, // 0xFD
-		{ 0, "cp",          }, // 0xFE
+		{ 0, "cp %02X",          }, // 0xFE
 		{ 0, "rst 38"       }  // 0xFF
 	};
 
