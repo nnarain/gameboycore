d9217a63952bc2158a7eddf04ecec657b1c41700
Natesh Narain <nnaraindev@gmail.com>
2016-09-12 19:52:32 -0400

    move tileset texture generation to its own class

--- src/gameboy/window.h
+++ src/gameboy/window.h
@@ -13,17 +13,16 @@
 
 #include <stdexcept>
 
+#include "tileset_texture.h"
+
 class Window
 {
 public:
 	Window(gb::TileRAM& tile_ram) :
 		window_(sf::VideoMode(250, 250), "Gameboy Emulator"),
-		tile_ram_(tile_ram)
+		tileset_(tile_ram)
 	{
-		if (!tileset_texture_.create(120, 120))
-			throw std::runtime_error("Could not create texture");
-
-		tileset_sprite_.setTexture(tileset_texture_);
+		tileset_sprite_.setTexture(tileset_.getTexture());
 		tileset_sprite_.setPosition(10, 10);
 	}
 
@@ -43,7 +42,7 @@ public:
 
 		window_.clear();
 
-		updateTilesetTexture();
+		tileset_.update();
 		window_.draw(tileset_sprite_);
 
 		window_.display();
@@ -59,77 +58,13 @@ public:
 	}
 
 private:
-	void drawTileset()
-	{
-		
-	}
 
-	void updateTilesetTexture()
-	{
-		std::vector<sf::Uint8> pixel_data(120 * 120 * 4, 255);
-		
-		std::vector<gb::Tile> tiles = tile_ram_.getTiles();
 
-		int tile_x     = 0;
-		int tile_y     = 0;
-		int tile_count = 0;
-
-		for (gb::Tile& tile : tiles)
-		{
-			int pixel = 0;
-			for (int row = 0; row < 8; ++row)
-			{
-				for (int col = 0; col < 8; ++col)
-				{
-					sf::Color color = getColor(tile.color[pixel++]);
-					writePixel(pixel_data, tile_x + col, tile_y + row, color);
-				}
-			}
-
-			tile_x += 8;
-			tile_count++;
-			if (tile_count >= 15)
-			{
-				tile_y += 8;
-				tile_x     = 0;
-				tile_count = 0;
-			}
-		}
-
-		// update the tileset texture
-		tileset_texture_.update(&pixel_data[0]);
-	}
-
-	void writePixel(std::vector<sf::Uint8>& pixels, int x, int y, sf::Color c)
-	{
-		const unsigned int pixels_per_row = 120; // pixels per row
-
-		int byte_offset = ((y * pixels_per_row) + x) * 4;
-
-		pixels[byte_offset + 0] = c.r;
-		pixels[byte_offset + 1] = c.g;
-		pixels[byte_offset + 2] = c.b;
-		pixels[byte_offset + 3] = c.a;
-	}
-
-	sf::Color getColor(uint8_t idx)
-	{
-		static sf::Color colors[] = {
-			{255, 255, 255, 255},
-			{192, 192, 192, 255},
-			{ 96,  96,  96, 255},
-			{  0,   0,   0, 255}
-		};
-
-		return colors[idx];
-	}
 
 private:
 	sf::RenderWindow window_;
 	
-
-	gb::TileRAM tile_ram_;
-	sf::Texture tileset_texture_;
+	TilesetTexture tileset_;
 	sf::Sprite tileset_sprite_;
 
 };
