5b419d52f14aadfdab19b53bc022b470e7d24d7d
Natesh Narain <nnaraindev@gmail.com>
2016-09-25 17:21:01 -0400

    changed window size

--- src/gameboy/window.h
+++ src/gameboy/window.h
@@ -22,7 +22,7 @@ class Window
 {
 public:
 	Window(gb::Gameboy& gameboy) :
-		window_(sf::VideoMode(325, 300), "Tis Emulator"),
+		window_(sf::VideoMode(320, 297), "Tis Emulator"),
 		screen_renderer_{gameboy}
 	{
 	}
