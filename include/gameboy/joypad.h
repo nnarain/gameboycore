ff9f0d4c285644a76765e7f5946159e970f28f8f
Natesh Narain <nnaraindev@gmail.com>
2016-10-08 17:13:30 -0400

    added source files

--- include/gameboy/joypad.h
+++ include/gameboy/joypad.h
@@ -28,46 +28,16 @@ namespace gb
 			START  = 7
 		};
 
-		Joypad(MMU& mmu) : 
-			mmu_(mmu),
-			reg_(mmu.get(memorymap::JOYPAD_REGISTER)),
-			keys_(0xFF),
-			interrupt_provider_(mmu, InterruptProvider::Interrupt::JOYPAD)
-		{
-			// add handlers
-			mmu_.addReadHandler(memorymap::JOYPAD_REGISTER, std::bind(&Joypad::readJoypad, this));
-			mmu_.addWriteHandler(memorymap::JOYPAD_REGISTER, std::bind(&Joypad::writeJoypad, this, std::placeholders::_1));
-		}
+		Joypad(MMU& mmu);
+		~Joypad();
 
-		void press(Key key)
-		{
-			keys_ &= ~(1 << static_cast<uint8_t>(key));
-			interrupt_provider_.set();
-		}
+		void press(Key key);
 
-		void release(Key key)
-		{
-			keys_ |= (1 << static_cast<uint8_t>(key));
-		}
+		void release(Key key);
 
-		uint8_t readJoypad()
-		{
-			// first 2 bits of high nybble is group selection
-			uint8_t group = ((~(reg_ >> 4)) & 0x03) - 1;
+		uint8_t readJoypad();
 
-			uint8_t selection = (keys_ >> (group * 4)) & 0x0F;
-
-			return (reg_ & 0xF0) | selection;
-		}
-
-		void writeJoypad(uint8_t value)
-		{
-			reg_ = value | 0x0F;
-		}
-
-		~Joypad()
-		{
-		}
+		void writeJoypad(uint8_t value);
 
 	private:
 		MMU& mmu_;
--- include/gameboy/tilemap.h
+++ include/gameboy/tilemap.h
@@ -20,6 +20,7 @@ namespace gb
 	class TileMap
 	{
 	public:
+		/* Map types */
 		enum class Map
 		{
 			BACKGROUND, WINDOW_OVERLAY
@@ -27,60 +28,11 @@ namespace gb
 
 	public:
 
-		TileMap(const TileRAM& tileram, const MMU& mmu, const LCDController& lcd) : 
-			tileram_(tileram),
-			mmu_(mmu),
-			lcd_(lcd)
-		{
-		}
+		TileMap(const TileRAM& tileram, const MMU& mmu, const LCDController& lcd);
+		~TileMap();
 
-		std::vector<Tile> getMapData(TileRAM tileram, Map map) const
-		{
-			// simplify this!
-
-			if (map == Map::BACKGROUND)
-			{
-				if (lcd_.getBackgroundMapLocation() == LCDController::BackgroundMapData::BG_DATA_1)
-				{
-					return getMapData(tileram, memorymap::BG_MAP_DATA_1_START, memorymap::BG_MAP_DATA_1_END);//Iterator(memorymap::BG_MAP_DATA_1_START, memorymap::BG_MAP_DATA_1_END, tileram_, mmu_);
-				}
-				else
-				{
-					return getMapData(tileram, memorymap::BG_MAP_DATA_2_START, memorymap::BG_MAP_DATA_2_END);//Iterator(memorymap::BG_MAP_DATA_2_START, memorymap::BG_MAP_DATA_2_END, tileram_, mmu_);
-				}
-			}
-			else // map == WINDOW_OVERLAY
-			{
-				if (lcd_.getWindowOverlayLocation() == LCDController::BackgroundMapData::BG_DATA_1)
-				{
-					return getMapData(tileram, memorymap::BG_MAP_DATA_1_START, memorymap::BG_MAP_DATA_1_END);//Iterator(memorymap::BG_MAP_DATA_1_START, memorymap::BG_MAP_DATA_1_END, tileram_, mmu_);
-				}
-				else
-				{
-					return getMapData(tileram, memorymap::BG_MAP_DATA_2_START, memorymap::BG_MAP_DATA_2_END); //Iterator(memorymap::BG_MAP_DATA_2_START, memorymap::BG_MAP_DATA_2_END, tileram_, mmu_);
-				}
-			}
-		}
-
-		std::vector<Tile> getMapData(TileRAM tileram, uint16_t start, uint16_t end) const
-		{
-			std::vector<Tile> tiles;
-
-			for (auto addr = start; addr <= end; ++addr)
-			{
-				// read the tile number from memory
-				auto tilenum = mmu_.read(addr);
-
-				// fetch from tile ram and push onto vector
-				tiles.push_back(tileram.getTile(tilenum));
-			}
-
-			return tiles;
-		}
-
-		~TileMap()
-		{
-		}
+		std::vector<Tile> getMapData(TileRAM tileram, Map map) const;
+		std::vector<Tile> getMapData(TileRAM tileram, uint16_t start, uint16_t end) const;
 
 	private:
 		TileRAM tileram_;
