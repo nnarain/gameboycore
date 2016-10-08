0cd3e15ad4edb84acb7f90ce25278f96923efacf
Natesh Narain <nnaraindev@gmail.com>
2016-10-08 16:31:22 -0400

    remove the iterator class

--- include/gameboy/tilemap.h
+++ include/gameboy/tilemap.h
@@ -25,49 +25,6 @@ namespace gb
 			BACKGROUND, WINDOW_OVERLAY
 		};
 
-		class Iterator
-		{
-		public:
-
-			Iterator(uint16_t begin, uint16_t end, TileRAM& tileram, const MMU& mmu) :
-				current_(begin),
-				end_(end),
-				tileram_(tileram),
-				mmu_(mmu)
-			{
-			}
-
-			Tile operator*()
-			{
-				return tileram_.getTile(mmu_.read(current_));
-			}
-
-			Iterator& operator++()
-			{
-				current_++;
-				return *this;
-			}
-
-			bool operator==(const Iterator& rhs)
-			{
-				// iterators are at the same memory location
-				return this->current_ == rhs.current_;
-			}
-
-			bool operator!=(const Iterator& rhs)
-			{
-				return !(*this == rhs);
-			}
-
-		public:
-			uint16_t current_;
-			uint16_t end_;
-
-		private:
-			TileRAM tileram_;
-			const MMU& mmu_;
-		};
-
 	public:
 
 		TileMap(const TileRAM& tileram, const MMU& mmu, const LCDController& lcd) : 
@@ -119,58 +76,6 @@ namespace gb
 			}
 
 			return tiles;
-		}
-
-		Iterator begin(Map map)
-		{
-			if (map == Map::BACKGROUND)
-			{
-				if (lcd_.getBackgroundMapLocation() == LCDController::BackgroundMapData::BG_DATA_1)
-				{
-					return Iterator(memorymap::BG_MAP_DATA_1_START, memorymap::BG_MAP_DATA_1_END, tileram_, mmu_);
-				}
-				else
-				{
-					return Iterator(memorymap::BG_MAP_DATA_2_START, memorymap::BG_MAP_DATA_2_END, tileram_, mmu_);
-				}
-			}
-			else // map == WINDOW_OVERLAY
-			{
-				if (lcd_.getWindowOverlayLocation() == LCDController::BackgroundMapData::BG_DATA_1)
-				{
-					return Iterator(memorymap::BG_MAP_DATA_1_START, memorymap::BG_MAP_DATA_1_END, tileram_, mmu_);
-				}
-				else
-				{
-					return Iterator(memorymap::BG_MAP_DATA_2_START, memorymap::BG_MAP_DATA_2_END, tileram_, mmu_);
-				}
-			}
-		}
-
-		Iterator end(Map map)
-		{
-			if (map == Map::BACKGROUND)
-			{
-				if (lcd_.getBackgroundMapLocation() == LCDController::BackgroundMapData::BG_DATA_1)
-				{
-					return Iterator(memorymap::BG_MAP_DATA_1_END, memorymap::BG_MAP_DATA_1_END, tileram_, mmu_);
-				}
-				else
-				{
-					return Iterator(memorymap::BG_MAP_DATA_2_END, memorymap::BG_MAP_DATA_2_END, tileram_, mmu_);
-				}
-			}
-			else // map == WINDOW_OVERLAY
-			{
-				if (lcd_.getBackgroundMapLocation() == LCDController::BackgroundMapData::BG_DATA_1)
-				{
-					return Iterator(memorymap::BG_MAP_DATA_1_END, memorymap::BG_MAP_DATA_1_END, tileram_, mmu_);
-				}
-				else
-				{
-					return Iterator(memorymap::BG_MAP_DATA_2_END, memorymap::BG_MAP_DATA_2_END, tileram_, mmu_);
-				}
-			}
 		}
 
 		~TileMap()
