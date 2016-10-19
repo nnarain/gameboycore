ef688644ac14f523aaa7c6576216a3bd42b79d86
Natesh Narain <nnaraindev@gmail.com>
2016-10-18 22:14:35 -0400

    fix timer interrupt

--- include/gameboy/timer.h
+++ include/gameboy/timer.h
@@ -24,10 +24,11 @@ namespace gb
 
 	public:
 		Timer(MMU& mmu) :
+			controller_(mmu.get(memorymap::TIMER_CONTROLLER_REGISTER)),
 			counter_(mmu.get(memorymap::TIMER_COUNTER_REGISTER)),
 			modulo_ (mmu.get(memorymap::TIMER_MODULO_REGISTER)),
 			is_enabled_(false),
-			clock_period_(0),
+			count_to_tick_(0),
 			cycle_count_(0),
 			timer_interrupt_(mmu, InterruptProvider::Interrupt::TIMER)
 		{
@@ -40,36 +41,35 @@ namespace gb
 
 			cycle_count_ += cycles;
 
-			// use elapsed clock cycles to determine time past
-			double elapsed = ((double)cycle_count_) * (1.f / SYSTEM_CLOCK);
-
-			// if time elapsed in greater than the clock period, increment the counter
-			if (elapsed >= clock_period_)
+			if (cycle_count_ >= count_to_tick_)
 			{
-				// if the counter is going to overflow, set timer overflow interrupt and load modulo register
 				if (counter_ == 0xFF)
 				{
 					counter_ = modulo_;
 					timer_interrupt_.set();
+					cycle_count_ = 0;
 				}
 
+				count_to_tick_ = 0;
 				counter_++;
-				cycle_count_ = 0;
 			}
 		}
 
 		void configure(uint8_t value)
 		{
-			constexpr double clock_modes[] = {
-				1.f / 4096.f,
-				1.f / 262144.f,
-				1.f / 65536.f,
-				1.f / 16384.f
+			static constexpr uint32_t SYTSEM_FREQ = 4000000u;
+			static constexpr uint32_t freq_selection[] = {
+				0x4096u,
+				0x262144u,
+				0x65536u,
+				0x16384u
 			};
 
 			is_enabled_ = (value & 0x04) != 0;
 
-			clock_period_ = clock_modes[value & 0x03];
+			count_to_tick_ = SYTSEM_FREQ / freq_selection[value & 0x03];
+
+			controller_ = value;
 		}
 
 		~Timer()
@@ -77,11 +77,12 @@ namespace gb
 		}
 
 	private:
+		uint8_t& controller_;
 		uint8_t& counter_;
 		uint8_t& modulo_;
 
 		bool is_enabled_;
-		double clock_period_;
+		uint32_t count_to_tick_;
 
 		uint32_t cycle_count_;
 
