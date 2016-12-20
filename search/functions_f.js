dc2f87264269b5e4dd1fff61b89c4b96ea0b11b5
Natesh Narain <nnaraindev@gmail.com>
2016-12-20 02:41:41 +0000

    Documentation

--- search/functions_f.js
+++ search/functions_f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['update',['update',['../classgb_1_1APU.html#a5194fcb2eda6fdaf9c39872bd42abb6e',1,'gb::APU::update()'],['../classgb_1_1GameboyCore.html#aa670cbfe475004108b3e6b41d9f918b4',1,'gb::GameboyCore::update()'],['../classgb_1_1GPU.html#a50dd6912a5f2546d64acbad5ea72de94',1,'gb::GPU::update()'],['../classgb_1_1Link.html#a2e4a4f1c6c4315a71a5f74661f2fadc9',1,'gb::Link::update()'],['../classgb_1_1LinkCable.html#aa5df4509cc656294347e2ee8e54f350b',1,'gb::LinkCable::update()'],['../classgb_1_1Timer.html#a3c26e3a48f25fa501597353ffdcbc1dc',1,'gb::Timer::update()']]]
+  ['update',['update',['../classgb_1_1APU.html#a5194fcb2eda6fdaf9c39872bd42abb6e',1,'gb::APU::update()'],['../classgb_1_1GameboyCore.html#aa670cbfe475004108b3e6b41d9f918b4',1,'gb::GameboyCore::update()'],['../classgb_1_1GPU.html#a50dd6912a5f2546d64acbad5ea72de94',1,'gb::GPU::update()'],['../classgb_1_1Link.html#a2e4a4f1c6c4315a71a5f74661f2fadc9',1,'gb::Link::update()'],['../classgb_1_1Timer.html#a3c26e3a48f25fa501597353ffdcbc1dc',1,'gb::Timer::update()']]]
 ];
