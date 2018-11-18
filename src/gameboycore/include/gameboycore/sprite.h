e0583ed41647d2cca1bca0913220a6e1eb9cd2cf
Natesh Narain <nnaraindev@gmail.com>
2018-11-18 17:24:05 -0500

    Added unit test for getting sprites

--- src/gameboycore/include/gameboycore/sprite.h
+++ src/gameboycore/include/gameboycore/sprite.h
@@ -1,6 +1,6 @@
 
 /**
-	@author Natesh Narain <nnaraindev@gmail.com>
+    @author Natesh Narain <nnaraindev@gmail.com>
 */
 
 #ifndef GAMEBOYCORE_SPRITE_H
@@ -10,59 +10,67 @@
 
 namespace gb
 {
-	/**
-		Sprite object that are stored in OAM
-	*/
-	class Sprite
-	{
-	public:
-		uint8_t y;    ///< y pixel coordinate
-		uint8_t x;    ///< x pixel coordinate
-		uint8_t tile; ///< tile number
-		uint8_t attr; ///< attribute data
+    /**
+        Sprite object that are stored in OAM
+    */
+    class Sprite
+    {
+    public:
+        uint8_t y;    ///< y pixel coordinate
+        uint8_t x;    ///< x pixel coordinate
+        uint8_t tile; ///< tile number
+        uint8_t attr; ///< attribute data
 
-		uint8_t height; ///< sprite height in pixels
+        uint8_t height; ///< sprite height in pixels
 
-		constexpr bool isHorizontallyFlipped() const
-		{
-			return (attr & (1 << 5)) != 0;
-		}
+        Sprite()
+            : y{0}
+            , x{0}
+            , tile{0}
+            , attr{0}
+        {
+        }
 
-		constexpr bool isVerticallyFlipped() const
-		{
-			return (attr & (1 << 6)) != 0;
-		}
+        constexpr bool isHorizontallyFlipped() const
+        {
+            return (attr & (1 << 5)) != 0;
+        }
 
-		constexpr bool hasPriority() const
-		{
-			return (attr & (1 << 7)) == 0;
-		}
+        constexpr bool isVerticallyFlipped() const
+        {
+            return (attr & (1 << 6)) != 0;
+        }
 
-		constexpr uint8_t paletteOBP0() const
-		{
-			return !!(attr & (1 << 4));
-		}
+        constexpr bool hasPriority() const
+        {
+            return (attr & (1 << 7)) == 0;
+        }
 
-		constexpr uint8_t getCgbPalette() const
-		{
-			return (attr & 0x03);
-		}
+        constexpr uint8_t paletteOBP0() const
+        {
+            return !!(attr & (1 << 4));
+        }
 
-		constexpr uint8_t getCharacterBank() const
-		{
-			return !!(attr & (1 << 3));
-		}
+        constexpr uint8_t getCgbPalette() const
+        {
+            return (attr & 0x07);
+        }
 
-		bool operator==(const Sprite& rhs)
-		{
-			return
-        this->y == rhs.y &&
-        this->x == rhs.x &&
-        this->tile == rhs.tile &&
-        this->attr == rhs.attr &&
-        this->height == rhs.height;
-		}
-	};
+        constexpr uint8_t getCharacterBank() const
+        {
+            return !!(attr & (1 << 3));
+        }
+
+        bool operator==(const Sprite& rhs)
+        {
+            return
+                this->y == rhs.y &&
+                this->x == rhs.x &&
+                this->tile == rhs.tile &&
+                this->attr == rhs.attr &&
+                this->height == rhs.height;
+        }
+    };
 }
 
 #endif // GAMEBOYCORE_SPRITE_H
--- src/gameboycore/tests/test_api.cpp
+++ src/gameboycore/tests/test_api.cpp
@@ -4,6 +4,7 @@
 #include "util/codegenerator.h"
 
 #include <gameboycore/gameboycore.h>
+#include <gameboycore/memorymap.h>
 
 using namespace gb;
 
@@ -58,4 +59,89 @@ TEST(API, CallbackAfterReset)
     core.emulateFrame();
 
     EXPECT_TRUE(vblank_fired);
+}
+
+TEST(API, SpriteAttributes)
+{
+    /**
+        OAM Registers - 4 bytes per sprite
+
+        0 - y coord
+        1 - x coord
+        2 - CHR code (tile number)
+        3 - Attributes
+            Bits:
+                7     - Priority
+                6     - Vertical flip
+                5     - Horizontal flip
+                4     - DMG palette
+                3     - CBG character bank
+                2,1,0 - CGB palette
+    */
+
+    CodeGenerator code;
+    code.block(
+        0x3E, 0x82,      // LD A, $02 -> LCDC enable | sprites enabled
+        0xE0, 0x40,      // LDH ($40), A
+        0xC3, 0x54, 0x01 // JP $0154
+    );
+
+    // initialize the core
+    GameboyCore core;
+
+    auto rom = code.rom();
+    core.loadROM(&rom[0], rom.size());
+
+    // write sprites into memory
+    core.writeMemory(memorymap::OAM_START + 0, 8);
+    core.writeMemory(memorymap::OAM_START + 1, 8);
+    core.writeMemory(memorymap::OAM_START + 2, 1);
+    core.writeMemory(memorymap::OAM_START + 3, 0x20); // horizontal flip
+
+    core.writeMemory(memorymap::OAM_START + 4, 0);
+    core.writeMemory(memorymap::OAM_START + 5, 0);
+    core.writeMemory(memorymap::OAM_START + 6, 2);
+    core.writeMemory(memorymap::OAM_START + 7, 0x40); // vertical flip
+
+    core.writeMemory(memorymap::OAM_START + 8, 0);
+    core.writeMemory(memorymap::OAM_START + 9, 0);
+    core.writeMemory(memorymap::OAM_START + 10, 3);
+    core.writeMemory(memorymap::OAM_START + 11, 0x80); // display priority
+
+    core.writeMemory(memorymap::OAM_START + 12, 0);
+    core.writeMemory(memorymap::OAM_START + 13, 0);
+    core.writeMemory(memorymap::OAM_START + 14, 0);
+    core.writeMemory(memorymap::OAM_START + 15, 0x07); // CGB palette 7
+
+    core.writeMemory(memorymap::OAM_START + 16, 0);
+    core.writeMemory(memorymap::OAM_START + 17, 0);
+    core.writeMemory(memorymap::OAM_START + 18, 0);
+    core.writeMemory(memorymap::OAM_START + 19, 0x10); // DMG palette
+
+    // emulate a frame to populate sprite cache
+    core.emulateFrame();
+
+    // get sprites
+    auto sprites = core.getGPU()->getSpriteCache();
+
+    const auto& s0 = sprites[0];
+    const auto& s1 = sprites[1];
+    const auto& s2 = sprites[2];
+    const auto& s3 = sprites[3];
+    const auto& s4 = sprites[4];
+
+    EXPECT_EQ(s0.x, 8);
+    EXPECT_EQ(s0.y, 8);
+    EXPECT_EQ(s0.tile, 1);
+    EXPECT_EQ(s0.isHorizontallyFlipped(), true);
+    EXPECT_EQ(s0.isVerticallyFlipped(), false);
+
+    EXPECT_EQ(s1.isHorizontallyFlipped(), false);
+    EXPECT_EQ(s1.isVerticallyFlipped(), true);
+
+    EXPECT_EQ(!s2.hasPriority(), true);
+
+    EXPECT_EQ(s3.getCgbPalette(), 0x07);
+
+    EXPECT_EQ(s4.paletteOBP0(), 1);
 }
