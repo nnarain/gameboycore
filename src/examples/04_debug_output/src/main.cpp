8ee4a60bf65e574ecdfd5b9dfa4d47c6a0738820
Natesh Narain <nnaraindev@gmail.com>
2019-04-25 17:28:32 -0400

    debug output example

---
+++ src/examples/04_debug_output/src/main.cpp
@@ -0,0 +1,42 @@
+#include <gameboycore/gameboycore.h>
+
+#include <iostream>
+#include <functional>
+
+using namespace gb;
+
+void instructionCallback(const Instruction& instruction)
+{
+    std::cout << disassemble(instruction) << "\n";
+}
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Must provide path to a ROM file\n";
+        return 1;
+    }
+
+    std::string filepath{ argv[1] };
+
+    GameboyCore core;
+
+    // Open the ROM specified ROM file
+    try
+    {
+        core.open(filepath);
+
+        core.setDebugMode(true);
+        core.setInstructionCallback(instructionCallback);
+    }
+    catch (const std::runtime_error & e)
+    {
+        std::cerr << "Error loading ROM file: " << e.what() << "\n";
+        return 1;
+    }
+
+    core.emulateFrame();
+
+    return 0;
+}
